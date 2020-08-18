98c4b5f3dc07a228c004650625b727e75df32745
kc611 <bcs_2019029@iiitm.ac.in>
2020-08-18 15:20:03 +0530

    Heroku Init

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'urlshortener.wsgi.application'
 
 
 # Database
