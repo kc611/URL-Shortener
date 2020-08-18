a177f4533373124fbf4d512ce677abf35305b0ed
kc611 <bcs_2019029@iiitm.ac.in>
2020-08-18 14:46:42 +0530

    Initial Commit

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
